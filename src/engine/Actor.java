0a2ab6ba7f78edb4975b0749fd0387af9dbfaf17
Nandini Thathachari <nthathachari886@student.fuhsd.org>
2026-04-19 23:34:43 -0700

    added code to actor, struggling with intersecting objs

--- src/engine/Actor.java
+++ src/engine/Actor.java
@@ -1,5 +1,31 @@
 package engine;
 
-public abstract class Actor {
+import javafx.scene.image.ImageView;
 
+public abstract class Actor extends ImageView{
+
+	public Actor() {
+		
+	}
+	
+	public void move (double dx, double dy) {
+		setX(getX()+dx);
+		setY(getY()+dy);
+	}
+	
+	public World getWorld() {
+		return (World)getParent();
+	}
+	
+	public double getWidth() {
+		return getBoundsInParent().getWidth();
+	}
+	
+	public double getHeight() {
+		return getBoundsInParent().getHeight();
+	}
+	
+	public <A extends Actor> java.util.List<A> getIntersectingObjects(java.lang.Class<A> cls) {
+		List a = new 
+	}
 }
